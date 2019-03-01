c04e84d4f9ccd2c811d04dc682802681b9cc8c7f
Nick Vennarucci <nickvenn@comcast.net>
2019-02-28 18:31:34 -0800

    hi

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/PremierLeagueNews", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/PremierLeagueNews";
+
+mongoose.connect(MONGODB_URI);
+
 
 // Routes
 app.get("/", function(req, res){
